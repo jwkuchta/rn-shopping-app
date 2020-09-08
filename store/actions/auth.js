1dfe7b306cc7e49aec8b994d4c0d66176760e86d
Justina Kuchta <justyna.kuchta@gmail.com>
2020-09-08 09:58:14 -0400

    login works and accounts for common errors

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,110 +1,59 @@
-// export const SIGNUP = 'SIGNUP'
-// export const LOGIN = 'LOGIN'
-// import { FIREBASE_API_KEY as apiKey} from '../../constants/_api_keys'
+export const SIGNUP = 'SIGNUP'
+export const LOGIN = 'LOGIN'
+import { FIREBASE_API_KEY as apiKey} from '../../constants/_api_keys'
 
-// const authSignupUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`
-// const authLoginUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`
-
-// export const signup = (email, password) => {
-//     return async dispatch => {
-//         const response = await fetch(authSignupUrl, {
-//             method: 'POST',
-//             headers: {
-//                 'content-type': 'application/json'
-//             },
-//             body: JSON.stringify({
-//                 email: email, 
-//                 password: password,
-//                 returnSecureToken: true
-//             })
-//         })
-//         if (!response.ok) {
-//             throw new Error('something went wrong')
-//         }
-//         const resData = await response.json()
-//         console.log(resData)
-//         dispatch({ type: SIGNUP })
-//     }
-// }
-
-// export const login = (email, password) => {
-//     console.log('in the login action')
-//     return async dispatch => {
-//         const response = await fetch(authSignupUrl, {
-//             method: 'POST',
-//             headers: {
-//                 'content-type': 'application/json'
-//             },
-//             body: JSON.stringify({
-//                 email: email, 
-//                 password: password,
-//                 returnSecureToken: true
-//             })
-//         })
-//         console.log('response from login action', response.json())
-//         if (!response.ok) {
-//             throw new Error('something went wrong')
-//         }
-//         const resData = await response.json()
-//         console.log(resData)
-//         dispatch({ type: LOGIN })
-//     }
-// }
-
-
-export const SIGNUP = 'SIGNUP';
-export const LOGIN = 'LOGIN';
+const authSignupUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`
+const authLoginUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`
 
 export const signup = (email, password) => {
-  return async dispatch => {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBY8UJq_xLD0nEe1HZHuvEOUfYIS9gg4pA',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-          returnSecureToken: true
+    return async dispatch => {
+        const response = await fetch(authSignupUrl, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email, 
+                password: password,
+                returnSecureToken: true
+            })
         })
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error('Something went wrong!');
+        if (!response.ok) {
+            throw new Error('something went wrong')
+        }
+        const resData = await response.json()
+        console.log(resData)
+        dispatch({ type: SIGNUP })
     }
-
-    const resData = await response.json();
-    console.log(resData);
-    dispatch({ type: SIGNUP });
-  };
-};
+}
 
 export const login = (email, password) => {
     return async dispatch => {
-      const response = await fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBY8UJq_xLD0nEe1HZHuvEOUfYIS9gg4pA',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-            returnSecureToken: true
-          })
+        const response = await fetch(authLoginUrl, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email, 
+                password: password,
+                returnSecureToken: true
+            })
+        })
+        if (!response.ok) {
+            const errorResData = await response.json()
+            const errorId = errorResData.error.message;
+            let message = 'Something went wrong!';
+            if (errorId === 'EMAIL_NOT_FOUND') {
+                message = 'This email could not be found!';
+            } else if (errorId === 'INVALID_PASSWORD') {
+                message = 'This password is not valid!';
+            }
+        throw new Error(message)
         }
-      );
-  
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
-  
-      const resData = await response.json();
-      console.log(resData);
-      dispatch({ type: LOGIN });
-    };
-  };
+        const resData = await response.json()
+        console.log(resData)
+        dispatch({ type: LOGIN })
+    }
+}
+
